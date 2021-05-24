f301596e5cc208d2508a0b1113ac4a2bf0df30da
Simo Tumelius <simo.tumelius@gmail.com>
2021-05-24 13:11:29 +0300

    Format SQL in accordance with SQLFluff configs (#2)

--- models/customers.sql
+++ models/customers.sql
@@ -1,64 +1,58 @@
 with customers as (
-
-    select * from {{ ref('stg_customers') }}
-
+  select
+    *
+  from {{ ref('stg_customers') }}
 ),
 
 orders as (
-
-    select * from {{ ref('stg_orders') }}
-
+  select
+    *
+  from {{ ref('stg_orders') }}
 ),
 
 payments as (
-
-    select * from {{ ref('stg_payments') }}
-
+  select
+    *
+  from {{ ref('stg_payments') }}
 ),
 
 customer_orders as (
+  select
+    customer_id,
 
-        select
-        customer_id,
-
-        min(order_date) as first_order,
-        max(order_date) as most_recent_order,
-        count(order_id) as number_of_orders
-    from orders
-
-    group by 1
+    min(order_date) as first_order,
+    max(order_date) as most_recent_order,
+    count(order_id) as number_of_orders
+  from orders
+  group by 1
 
 ),
 
 customer_payments as (
-
-    select
-        orders.customer_id,
-        sum(amount) as total_amount
-
-    from payments
-
-    left join orders using (order_id)
-
-    group by 1
-
+  select
+    orders.customer_id,
+    sum(payments.amount) as total_amount
+  from payments
+  left join orders on (payments.order_id = orders.order_id)
+  group by 1
 ),
 
 final as (
-
-    select
-        customers.customer_id,
-        customer_orders.first_order,
-        customer_orders.most_recent_order,
-        customer_orders.number_of_orders,
-        customer_payments.total_amount as customer_lifetime_value
-
-    from customers
-
-    left join customer_orders using (customer_id)
-
-    left join customer_payments using (customer_id)
-
+  select
+    customers.customer_id,
+    customer_orders.first_order,
+    customer_orders.most_recent_order,
+    customer_orders.number_of_orders,
+    customer_payments.total_amount as customer_lifetime_value
+  from customers
+  left join customer_orders on (
+    customers.customer_id = customer_orders.customer_id
+  )  -- noqa: L003
+  left join customer_payments on (
+    customers.customer_id = customer_payments.customer_id
+  )  -- noqa: L003
 )
 
-select * from final
+select
+  *
+from final
