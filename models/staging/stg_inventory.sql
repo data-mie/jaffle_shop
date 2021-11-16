4eed284578cadac1c6e3e8b23f17467a890869da
Simo Tumelius <simo@datamie.fi>
2021-11-16 19:05:04 +0200

    Add raw_inventory seed table and stg_inventory model

---
+++ models/staging/stg_inventory.sql
@@ -0,0 +1,22 @@
+with source as (
+  {#-
+  Normally we would select from the table here, but we are using seeds to load
+  our data in this project
+  #}
+  select
+    *
+  from {{ ref('raw_inventory') }}
+),
+
+renamed as (
+  select
+    id as inventory_id,
+    sku,
+    inventory_on_hand,
+    updated_at
+  from source
+)
+
+select
+  *
+from renamed
