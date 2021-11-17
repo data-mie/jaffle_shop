f26b693ae2c9724f35dd0be8c37001c7458a55dc
Simo Tumelius <simo@datamie.fi>
2021-11-17 06:37:56 +0000

    Update raw_inventory

--- snapshots/inventory_snapshot.sql
+++ snapshots/inventory_snapshot.sql
@@ -5,10 +5,11 @@
         unique_key='id',
         strategy='timestamp',
         updated_at='updated_at',
-        target_schema='snapshots'
+        target_schema='snapshots',
+        invalidate_hard_deletes=True
     )
 }}
 
-select * from {{ ref('raw_inventory') }}
+select *, cast(date as timestamp) as updated_at from {{ ref('raw_inventory') }}
 
 {% endsnapshot %}
